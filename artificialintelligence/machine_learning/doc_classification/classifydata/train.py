677383ad225fbad90cfd070cde42a754e297a69c
ilanchezhian <ilanchezhian.j@concord.net>
2021-02-21 19:51:26 +0530

    sklearn notebooks

--- artificialintelligence/machine_learning/doc_classification/classifydata/train.py
+++ artificialintelligence/machine_learning/doc_classification/classifydata/train.py
@@ -3,7 +3,7 @@ import time
 
 import numpy as np
 import sklearn
-from sklearn.cross_validation import cross_val_score
+from sklearn.model_selection import cross_val_score
 from sklearn.datasets import load_files
 from sklearn.externals import joblib
 from sklearn.feature_extraction import stop_words
@@ -14,11 +14,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
-
+from sklearn.ensemble import RandomForestClassifier
 from artificialintelligence.machine_learning.doc_classification.classifydata.Helper import Test
 
-DATA_DIR = "/home/ila/Documents/abbyy/v3.1-segments/all (copy)/"
-# DATA_DIR = "/home/ila/Documents/900_docs/ocr_text/"
+DATA_DIR = "dataset_5classes"
 
 data = load_files(DATA_DIR, encoding="utf-8", decode_error="replace")
 #print("the classes are",data.target_names)
@@ -49,29 +48,31 @@ print("keywords=",keywords)
 #print("keywords=",keywords)
 #print("keywords size=",len(keywords))
 X_train_vectorized = vectorizer.transform(X_train)
-print("Stop Words=",vectorizer.stop_words_)
+# print("Stop Words=",vectorizer.stop_words_)
 with open("stopwords.txt", "w") as fptr:
     for word in vectorizer.stop_words_:
         fptr.write(word+" ")
     for word in stop_words.ENGLISH_STOP_WORDS:
         fptr.write(word + " ")
-    # fptr.write(str(vectorizer.stop_words_))
-# outfile = open("stopwords",'wb')
-# pickle.dump(vectorizer.stop_words_,outfile)
-# outfile.close()
+with open("features.txt", "w") as fptr:
+    for word in keywords:
+        fptr.write(word + " ")
+
 #naivebayes
 cls = MultinomialNB(alpha=0.001)
+cls = clf = RandomForestClassifier()
 cls.fit(vectorizer.transform(X_train), y_train)
-joblib.dump(cls, "" + 'model.h5')
-#print("type(X_test)=",type(X_test))
-#print("type(vectorizer.transform(X_test))=",type(vectorizer.transform(X_test)))
+joblib.dump(cls, "" + 'nb_model.h5')
 testobj=Test()
 test_doc=testobj.data_for_a_document()
-#prediction_for_a_doc=cls.predict(vectorizer.transform(test_doc))
-#print("pred for a doc ",prediction_for_a_doc)
-
 y_pred = cls.predict(vectorizer.transform(X_test))
-#print("accuracy with naive bayes = ",accuracy_score(y_test, y_pred))
+print("accuracy with naive bayes = ",accuracy_score(y_test, y_pred))
+#RandomForest
+clf = clf = RandomForestClassifier()
+clf.fit(vectorizer.transform(X_train), y_train)
+joblib.dump(clf, "" + 'rf_model.h5')
+y_pred = clf.predict(vectorizer.transform(X_test))
+print("accuracy with randomforest classifier = ",accuracy_score(y_test, y_pred))
 
 svc_tfidf = Pipeline([
     ("tfidf_vectorizer", TfidfVectorizer(stop_words="english", max_features=3000)),
@@ -81,4 +82,4 @@ models = [("svc_tfidf", svc_tfidf), ]
 
 svc_score = [(name, cross_val_score(model, X_train, y_train, cv=2).mean()) for name, model in models]
 scores = sorted(svc_score, key=lambda x: -x[1])
-#print(scores)
+print(scores)
